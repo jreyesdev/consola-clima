473a2a1e5f3ff346b765622f43e03fda776d9ed7
jreyesdev <djrm1204@gmail.com>
2021-09-05 21:44:44 -0400

    Metodo para consultar clima

--- models/busquedas.js
+++ models/busquedas.js
@@ -7,6 +7,8 @@ class Busquedas {
         // TODO: leer si BBDD existe
         this.mapBoxUrl = process.env.MAPBOX_URL
         this.mapBoxToken = process.env.MAPBOX_KEY
+        this.openWURL = process.env.OPENWEATHER_URL
+        this.openWKEY = process.env.OPENWEATHER_KEY
     }
 
     get paramsMapBox(){
@@ -17,13 +19,21 @@ class Busquedas {
         }
     }
 
+    get paramsOpenWeather(){
+        return {
+            'appid': this.openWKEY,
+            'lang': 'es',
+            'units': 'metric'
+        }
+    }
+
     async lugares(lugar=''){
         try{
             const instance = axios.create({
                 baseURL: `${this.mapBoxUrl}/${lugar}.json`,
                 params: this.paramsMapBox
-            });
-            const { data } = await instance.get();
+            })
+            const { data } = await instance.get()
             return data.features.map(place => ({
                 id: place.id,
                 nombre: place.place_name,
@@ -32,7 +42,24 @@ class Busquedas {
             }))
         }catch(err){
             console.error(err)
-            return []
+        }
+    }
+
+    async climaLugar(lat,lon){
+        try{
+            const instance = axios.create({
+                baseURL: this.openWURL,
+                params: { ...this.paramsOpenWeather, lat, lon }
+            })
+            const { data: { weather, main } } = await instance.get()
+            return {
+                desc: weather[0].description,
+                min: main.temp_min,
+                max: main.temp_max,
+                temp: main.temp
+            }
+        }catch(err){
+            console.error(err)
         }
     }
 }
